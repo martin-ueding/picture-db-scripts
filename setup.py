b2722308c45e0e835490698f34db4577c5a4eb4b
Martin Ueding <dev@martin-ueding.de>
2012-02-12 21:04:22 +0100

    Add setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# Copyright (c) 2012 Martin Ueding <dev@martin-ueding.de>
+
+from distutils.core import setup
+
+setup(
+    author = "Martin Ueding",
+    author_email = "dev@martin-ueding.de",
+    name = "picture_renamer",
+    scripts = ["picture_renamer"],
+    version = "1.1",
+)
