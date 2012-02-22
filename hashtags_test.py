460452c652b38e0420a6171d6df8b25b8d0b1f76
Martin Ueding <dev@martin-ueding.de>
2012-02-22 19:47:07 +0100

    Using self.assertEqual instead of assert

--- hashtags_test.py
+++ hashtags_test.py
@@ -10,27 +10,29 @@ from hashtags import *
 class ParseFilenameTest(unittest.TestCase):
     def test_1(self):
         name, tags = parse_filename("20120204-Klopapierberg-9240#Martin_Ueding.jpg")
-        assert name == '20120204-Klopapierberg-9240.jpg'
-        assert tags == [Tag('Martin Ueding')]
+        self.assertEqual(name, '20120204-Klopapierberg-9240.jpg')
+        self.assertEqual(tags, [Tag('Martin Ueding')])
 
     def test_2(self):
         name, tags = parse_filename("20120204-Klopapierberg-9240#Martin_Ueding#Another_Tag.jpg")
-        assert name == '20120204-Klopapierberg-9240.jpg'
-        assert tags == [Tag('Martin Ueding'), Tag('Another Tag')]
+        self.assertEqual(name, '20120204-Klopapierberg-9240.jpg')
+        self.assertEqual(tags, [Tag('Martin Ueding'), Tag('Another Tag')])
 
 
 class GenerateFilenameTest(unittest.TestCase):
     def test_1(self):
-        assert generate_filename('20120204-Klopapierberg-9240.jpg', [Tag('Martin_Ueding'), Tag('Another Tag')]) == '20120204-Klopapierberg-9240#Another_Tag#Martin_Ueding.jpg'
+        self.assertEqual(generate_filename('20120204-Klopapierberg-9240.jpg', [Tag('Martin_Ueding'), Tag('Another Tag')]), '20120204-Klopapierberg-9240#Another_Tag#Martin_Ueding.jpg')
 
     def test_2(self):
-        assert generate_filename('20120204-Klopapierberg-9240.jpg', []) == '20120204-Klopapierberg-9240.jpg'
+        self.assertEqual(generate_filename('20120204-Klopapierberg-9240.jpg', []), '20120204-Klopapierberg-9240.jpg')
 
     def test_3(self):
-        assert generate_filename('20120204-Klopapierberg-9240.jpg', [Tag('Martin Ueding')]) == '20120204-Klopapierberg-9240#Martin_Ueding.jpg'
+        self.assertEqual(generate_filename('20120204-Klopapierberg-9240.jpg', [Tag('Martin Ueding')]), '20120204-Klopapierberg-9240#Martin_Ueding.jpg')
 
     def test_4(self):
-        assert generate_filename('20120204-Klopapierberg-9240.jpg', [Tag('Martin Ueding'), Tag('Martin Ueding')]) == '20120204-Klopapierberg-9240#Martin_Ueding.jpg'
+        name = generate_filename('20120204-Klopapierberg-9240.jpg', [Tag('Martin Ueding'), Tag('Martin Ueding')])
+        self.assertEqual(name, '20120204-Klopapierberg-9240#Martin_Ueding.jpg')
+
 class ImageTest(unittest.TestCase):
     def test_currentpath(self):
         image = Image("20120204-Klopapierberg-9240.jpg")
