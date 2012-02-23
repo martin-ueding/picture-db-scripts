b24d6b46106ba8ceeaeebaba51b25c900837e012
Martin Ueding <dev@martin-ueding.de>
2012-02-23 13:13:10 +0100

    Fusion projects

--- makefile
+++ makefile
@@ -1,5 +1,12 @@
 # Copyright (c) 2012 Martin Ueding <dev@martin-ueding.de>
 
+pythonfiles := $(filter-out setup.py,$(wildcard *.py))
+testfiles := $(wildcard *_test.py)
+
+test:
+	python -m doctest $(pythonfiles)
+	python -m unittest $(testfiles:.py=)
+
 html/index.html: picturerenamer
 	epydoc -v $^
 
--- setup.py
+++ setup.py
@@ -6,7 +6,13 @@ from distutils.core import setup
 setup(
     author = "Martin Ueding",
     author_email = "dev@martin-ueding.de",
-    name = "picture_renamer",
-    scripts = ["picturerenamer"],
+    name = "picture_db_scripts",
+    py_modules = [
+        "hashtags",
+    ],
+    scripts = [
+        "hash-tags",
+        "picturerenamer",
+    ],
     version = "1.1",
 )
